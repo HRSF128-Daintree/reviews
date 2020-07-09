14c293a48b5b8f16ccbe91a722dfc08bfd8c6bcf
Watson Fu <watsonfu18@gmail.com>
2020-07-08 23:35:56 -0500

    Cleaned up for lint

--- server/controllers/reviews.js
+++ server/controllers/reviews.js
@@ -14,7 +14,7 @@ module.exports = {
     });
   },
   post(req, res) {
-    console.log('in post and body is ', req.body)
+    console.log('in post and body is ', req.body);
     models.createReview(req.body).then(() => {
       res.sendStatus(201);
     });
@@ -32,20 +32,3 @@ module.exports = {
     });
   },
 };
-
-// "user_id": 1513,
-// "hotel_id": 99,
-// "review_date": "2019-11-27T02:19:04.000Z",
-// "review_body": "I was delighted by the cupboards in the room. The location was noisy. I would likely stay here again.",
-// "date_of_stay": "2019-09-11T05:00:00.000Z",
-// "room_tip": "Never forget to try the snacks.",
-// "trip_type": "Couples",
-// "overall_rating": 3,
-// "value_rating": 5,
-// "location_rating": 2,
-// "service_rating": 2,
-// "rooms_rating": 1,
-// "cleanliness_rating": 5,
-// "sleep_quality_rating": 4,
-// "collected_in_part_hotel": 1,
-// "review_helpful_votes": 28
--- server/models/reviews.js
+++ server/models/reviews.js
@@ -1,12 +1,12 @@
 const Promise = require('bluebird');
-const db = require('../../database/index.js');
 const Moment = require('moment');
+const db = require('../../database/index.js');
 
 module.exports = {
   getReviews(hotelId) {
     return new Promise((resolve, reject) => {
       const sqlStr = 'SELECT reviews.*, month(reviews.date_of_stay) as month_of_stay, hotels.hotel_name, hotels.hotel_city, users.username, users.user_avatar, users.user_city, users.user_contributions, users.user_helpful_votes FROM reviews JOIN hotels ON reviews.hotel_id = hotels.id JOIN users ON reviews.user_id = users.id WHERE hotels.id = ?';
-      db.query(sqlStr, [hotelId], ((err, results, fields) => {
+      db.query(sqlStr, [hotelId], ((err, results) => {
         if (err) {
           reject(err);
         } else {
@@ -19,7 +19,7 @@ module.exports = {
   getOneReview(reviewId) {
     return new Promise((resolve, reject) => {
       const sqlStr = 'SELECT * from reviews WHERE id = ?';
-      db.query(sqlStr, [reviewId], ((err, results, fields) => {
+      db.query(sqlStr, [reviewId], ((err, results) => {
         if (err) {
           reject(err);
         } else {
@@ -36,9 +36,18 @@ module.exports = {
 
       const sqlStr = 'INSERT INTO reviews(user_id, hotel_id, review_date, review_body, date_of_stay, room_tip, trip_type, overall_rating, value_rating, location_rating, service_rating, rooms_rating, cleanliness_rating, sleep_quality_rating, collected_in_part_hotel, review_helpful_votes) VALUES ?';
 
-      const arr = [parseInt(data.user_id), parseInt(data.hotel_id), reviewDate, data.review_body, stayDate, data.room_tip, data.trip_type, parseInt(data.overall_rating), parseInt(data.value_rating), parseInt(data.location_rating), parseInt(data.service_rating), parseInt(data.rooms_rating), parseInt(data.cleanliness_rating), parseInt(data.sleep_quality_rating), parseInt(data.collected_in_part_hotel), parseInt(data.review_helpful_votes)];
+      const arr = [
+        parseInt(data.user_id, 10), parseInt(data.hotel_id, 10), reviewDate,
+        data.review_body, stayDate, data.room_tip, data.trip_type,
+        parseInt(data.overall_rating, 10), parseInt(data.value_rating, 10),
+        parseInt(data.location_rating, 10),
+        parseInt(data.service_rating, 10), parseInt(data.rooms_rating, 10),
+        parseInt(data.cleanliness_rating, 10),
+        parseInt(data.sleep_quality_rating, 10), parseInt(data.collected_in_part_hotel, 10),
+        parseInt(data.review_helpful_votes, 10),
+      ];
 
-      db.query(sqlStr, [[arr]], ((err, results, fields) => {
+      db.query(sqlStr, [[arr]], ((err, results) => {
         if (err) {
           reject(err);
         } else {
@@ -51,7 +60,7 @@ module.exports = {
   deleteReview(reviewId) {
     return new Promise((resolve, reject) => {
       const sqlStr = 'DELETE FROM reviews WHERE id = ?';
-      db.query(sqlStr, [reviewId], ((err, results, fields) => {
+      db.query(sqlStr, [reviewId], ((err, results) => {
         if (err) {
           reject(err);
         } else {
@@ -64,12 +73,12 @@ module.exports = {
 
   updateReview(data, reviewId) {
     return new Promise((resolve, reject) => {
-      const fieldVal = parseInt(data.fieldVal) ? parseInt(data.fieldVal) : data.fieldVal;
-      let fieldObj = {};
+      const fieldVal = parseInt(data.fieldVal, 10) ? parseInt(data.fieldVal, 10) : data.fieldVal;
+      const fieldObj = {};
       fieldObj[data.field] = fieldVal;
       const sqlStr = 'UPDATE reviews SET ? WHERE ?';
-      const params = [fieldObj, {id: reviewId}];
-      db.query(sqlStr, params, ((err, results, fields) => {
+      const params = [fieldObj, { id: reviewId }];
+      db.query(sqlStr, params, ((err, results) => {
         if (err) {
           reject(err);
         } else {
@@ -77,23 +86,5 @@ module.exports = {
         }
       }));
     });
-    // const reviewId = data.id || 2;
   },
 };
-
-// user_id: 1501,
-// hotel_id: 99,
-// review_date:2019-11-27,
-// review_body: "I was delighted by the cupboards in the room. The location was noisy. I would likely stay here again.",
-// date_of_stay:2019-09-11,
-// room_tip: "Never forget to try the snacks.",
-// trip_type: "Couples",
-// overall_rating: 3,
-// value_rating: 5,
-// location_rating: 2,
-// service_rating: 2,
-// rooms_rating: 1,
-// cleanliness_rating: 5,
-// sleep_quality_rating: 4,
-// collected_in_part_hotel: 1,
-// review_helpful_votes: 28
