339b9d6498b3e82fd9ceb14f3ea97ee97c23f4d7
Watson Fu <watsonfu18@gmail.com>
2020-07-17 14:09:54 -0500

    Added clusters

--- server/index.js
+++ server/index.js
@@ -1,19 +1,42 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cluster = require('cluster');
+const numCPUs = require('os').cpus().length;
 
-const app = express();
-const port = 3004;
-const controller = require('./controllers/reviews.js');
+if (cluster.isMaster) {
+  for (var i = 0; i < numCPUs; i++) {
+    // Create a worker
+    cluster.fork();
+  }
+} else {
+  const app = express();
+  const port = 3004;
+  const controller = require('./controllers/reviews.js');
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use('/reviews', express.static('public'));
+  app.use('/reviews', express.static('public'));
 
-app.get('/hotels/:hotelName', controller.getH);
-app.get('/users/:username', controller.getU);
-// app.post('/reviews', controller.post);
-// app.delete('/reviews/:reviewId', controller.delete);
-// app.patch('/reviews/:reviewId', controller.patch);
+  app.get('/hotels/:hotelName', controller.getH);
+  app.get('/users/:username', controller.getU);
 
-app.listen(port, () => console.log(`Reviews app listening at ${port}`));
+  app.listen(port, () => console.log(`Reviews app listening at ${port}`));
+}
+
+// const app = express();
+// const port = 3004;
+// const controller = require('./controllers/reviews.js');
+
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
+
+// app.use('/reviews', express.static('public'));
+
+// app.get('/hotels/:hotelName', controller.getH);
+// app.get('/users/:username', controller.getU);
+// // app.post('/reviews', controller.post);
+// // app.delete('/reviews/:reviewId', controller.delete);
+// // app.patch('/reviews/:reviewId', controller.patch);
+
+// app.listen(port, () => console.log(`Reviews app listening at ${port}`));
