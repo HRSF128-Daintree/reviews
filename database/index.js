54a8817ea49e730f39a65a50964971c525b80da4
Watson Fu <watsonfu18@gmail.com>
2020-07-17 12:15:52 -0500

    Rough get for reviews by hotel working with node driver

--- database/index.js
+++ database/index.js
@@ -5,10 +5,22 @@ const cassandra = require('cassandra-driver');
 const creds = require('../cassandra.creds.js');
 
 const client = new cassandra.Client({
-  contactPoints: ['h1', 'h2'],
+  contactPoints: ['127.0.0.1'],
   localDataCenter: 'datacenter1',
-  keyspace: 'ks1'
+  keyspace: 'reviewserver'
 });
+
+client.connect()
+  // .then(function () {
+  //   console.log('Connected to cluster with %d host(s): %j', client.hosts.length, client.hosts.keys());
+  //   console.log('Keyspaces: %j', Object.keys(client.metadata.keyspaces));
+  //   console.log('Shutting down');
+  //   return client.shutdown();
+  // })
+  .catch(function (err) {
+    console.error('There was an error when connecting', err);
+    return client.shutdown().then(() => { throw err; });
+  });
 
 // connection.connect((err) => {
 //   if (err) { console.error(`error: ${err.message}`); }
